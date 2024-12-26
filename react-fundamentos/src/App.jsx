b288a5b79283cc12d00c4a0fd29925ff27d83e50
Eladio Castañeda <eladio.dev@gmail.com>
2024-12-26 07:46:50 -0400

    se agregar funcionalidad e importes para correr un carrito sencillo que mestra con setState el agregar a cada item un articulo, comenzamos a trabajar en buenas practcas para componentes por separado

--- react-fundamentos/src/App.jsx
+++ react-fundamentos/src/App.jsx
@@ -1,26 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import React from 'react';
 
-function App() {
-  function TarjetaFruta (props) {
+/*function TarjetaFruta (props) {
     return (
       <div>
         <h3>{props.name}</h3>
         <hr />
         <p>Precio: ${props.price}</p>
       </div>
-    )
+    )*/ 
+
+class TarjetaFruta extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cantidad: 0
+    };
   }
 
+  render() {
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        <div>Cantidad: {this.state.cantidad}</div>
+        <button onClick={() => this.setState({ cantidad: this.state.cantidad + 1 })}>
+          Agregar
+        </button>
+        <hr />
+        <p>Precio: ${this.props.price}</p>
+      </div>
+    );
+  }
+}
+
+function App() {
   return (
     <div>
       <TarjetaFruta name="Manzana" price={10} />
-      <TarjetaFruta  name="Pera"  price={12} />
-      <TarjetaFruta  name="Platano" price={5} />
+      <TarjetaFruta name="Pera" price={12} />
+      <TarjetaFruta name="Plátano" price={5} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
